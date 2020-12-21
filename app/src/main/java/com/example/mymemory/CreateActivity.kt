91c68aeb1347a4edea4835cac669e4c6c56736d9
Harshita-Kanal <harshita.kgv@gmail.com>
2020-12-22 02:08:45 +0530

    Add logic for image save

--- app/src/main/java/com/example/mymemory/CreateActivity.kt
+++ app/src/main/java/com/example/mymemory/CreateActivity.kt
@@ -4,9 +4,16 @@ import android.app.Activity
 import android.content.ClipData
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
 import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
@@ -15,9 +22,11 @@ import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mymemory.models.BoardSize
+import com.example.mymemory.utils.BitmapScaler
 import com.example.mymemory.utils.EXTRA_BOARD_SIZE
 import com.example.mymemory.utils.isPermissionGranted
 import com.example.mymemory.utils.requestPermission
+import java.io.ByteArrayOutputStream
 
 class CreateActivity : AppCompatActivity() {
     companion object {
@@ -25,6 +34,9 @@ class CreateActivity : AppCompatActivity() {
         private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
         private const val READ_EXTERNAL_PHOTOS_CODE = 248
         private const val TAG = "CreateActivity"
+        private const val MAX_ALLOWABLE_LENGTH = 14
+        private  const val MIN_ALLOWABLE_LENGTH = 3
+
     }
     private lateinit var rvImagePicker: RecyclerView
     private lateinit var etGameName: EditText
@@ -32,6 +44,7 @@ class CreateActivity : AppCompatActivity() {
     private  lateinit var  adapter: ImagePickerAdapter
     private lateinit var boardSize: BoardSize
     private var numImagesRequired = -1
+
     private val chosenImageUris = mutableListOf<Uri>() //defines where the resource lives
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +57,28 @@ class CreateActivity : AppCompatActivity() {
         boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Choose pictures(0 / $numImagesRequired)"
+
+        //set on click listener
+        btnSave.setOnClickListener{
+            saveDataToFirebase()
+        }
+
+        //set a maximum allowable length
+        etGameName.filters = arrayOf(InputFilter.LengthFilter(MAX_ALLOWABLE_LENGTH))
+        etGameName.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                btnSave.isEnabled = shouldEnableSaveButton()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+        })
         //adapter and layout manager
         //context and list of images
         adapter = ImagePickerAdapter(this, chosenImageUris, boardSize, object: ImagePickerAdapter.ImageClickListener{
@@ -68,6 +103,29 @@ class CreateActivity : AppCompatActivity() {
         rvImagePicker.adapter = adapter
         rvImagePicker.setHasFixedSize(true)
         rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    private fun saveDataToFirebase() {
+        Log.d(TAG, "Added the image")
+        for((index, photoUri) in chosenImageUris.withIndex()){
+            val imageByteArray = getImageByteArray(photoUri)
+        }
+    }
+
+    private fun getImageByteArray(photoUri: Uri): ByteArray {
+        val originalBitmap = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+            val source = ImageDecoder.createSource(contentResolver, photoUri)
+            ImageDecoder.decodeBitmap(source)
+        }else{
+            MediaStore.Images.Media.getBitmap(contentResolver, photoUri)
+        }
+        Log.i(TAG, "Original Width ${originalBitmap.width}")
+        val scaledBitmap = BitmapScaler.scaleToFitHeight(originalBitmap, 250)
+        val byteOutputStream = ByteArrayOutputStream()
+        scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 60, byteOutputStream)
+        return byteOutputStream.toByteArray()
+
+
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
@@ -112,11 +170,19 @@ class CreateActivity : AppCompatActivity() {
         }
         adapter.notifyDataSetChanged()
         //update title of action bar
-        supportActionBar?.title = "Choose pictures(${chosenImageUris} / $numImagesRequired)"
+        supportActionBar?.title = "Choose pictures(${chosenImageUris.size} / $numImagesRequired)"
         btnSave.isEnabled = shouldEnableSaveButton()
     }
 
+
+    //enabling the save button
     private fun shouldEnableSaveButton(): Boolean {
+        if(chosenImageUris.size != numImagesRequired){
+            return false
+        }
+        if(etGameName.text.isBlank() || etGameName.text.length < MIN_ALLOWABLE_LENGTH){
+            return  false
+        }
          return true
     }
 
